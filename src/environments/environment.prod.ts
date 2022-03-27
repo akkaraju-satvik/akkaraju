5079cd51d9244c571bba8cbce3f54c602649b165
Akkaraju Satvik <satvik.akkaraju@gmail.com>
2022-03-27 11:27:37 +0530

    add angular fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'private-bruh',
+    appId: '1:293407460794:web:f45de8a8a827ed1604a183',
+    storageBucket: 'private-bruh.appspot.com',
+    apiKey: 'AIzaSyCMeB0zQM_Eqeuy7DnyL5QAuZYd8JaEhB0',
+    authDomain: 'private-bruh.firebaseapp.com',
+    messagingSenderId: '293407460794',
+  },
   production: true
 };
