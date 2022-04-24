4d81f5cdc728a245c2438023299377103d7b7433
Akkaraju Satvik <satvik.akkaraju@gmail.com>
2022-04-24 15:31:13 +0530

    change firebase project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   firebase: {
-    projectId: 'private-bruh',
-    appId: '1:293407460794:web:f45de8a8a827ed1604a183',
-    storageBucket: 'private-bruh.appspot.com',
-    apiKey: 'AIzaSyCMeB0zQM_Eqeuy7DnyL5QAuZYd8JaEhB0',
-    authDomain: 'private-bruh.firebaseapp.com',
-    messagingSenderId: '293407460794',
+    projectId: 'code-on-my-mind',
+    appId: '1:653443395788:web:797f093c4ab40e1317d269',
+    storageBucket: 'code-on-my-mind.appspot.com',
+    locationId: 'asia-south1',
+    apiKey: 'AIzaSyBbuIQvi347QrhsajM9zaLRLC-GRi9-reA',
+    authDomain: 'code-on-my-mind.firebaseapp.com',
+    messagingSenderId: '653443395788',
   },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,13 @@
 
 export const environment = {
   firebase: {
-    apiKey: "AIzaSyBbuIQvi347QrhsajM9zaLRLC-GRi9-reA",
-    authDomain: "code-on-my-mind.firebaseapp.com",
-    projectId: "code-on-my-mind",
-    storageBucket: "code-on-my-mind.appspot.com",
-    messagingSenderId: "653443395788",
-    appId: "1:653443395788:web:797f093c4ab40e1317d269"
+    projectId: 'code-on-my-mind',
+    appId: '1:653443395788:web:797f093c4ab40e1317d269',
+    storageBucket: 'code-on-my-mind.appspot.com',
+    locationId: 'asia-south1',
+    apiKey: 'AIzaSyBbuIQvi347QrhsajM9zaLRLC-GRi9-reA',
+    authDomain: 'code-on-my-mind.firebaseapp.com',
+    messagingSenderId: '653443395788',
   },
   production: false
 };
