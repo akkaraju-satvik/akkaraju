dc1c6edfc542c89b9afc8a00015c86b443c9ec65
Akkaraju Satvik <satvik.akkaraju@gmail.com>
2022-05-07 20:42:38 +0530

    update firebase project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    projectId: 'code-on-my-mind',
-    appId: '1:653443395788:web:797f093c4ab40e1317d269',
-    storageBucket: 'code-on-my-mind.appspot.com',
+    projectId: 'akkaraju-satvik',
+    appId: '1:17993737222:web:0a12cdc79d82ce5f128f87',
+    storageBucket: 'akkaraju-satvik.appspot.com',
     locationId: 'asia-south1',
-    apiKey: 'AIzaSyBbuIQvi347QrhsajM9zaLRLC-GRi9-reA',
-    authDomain: 'code-on-my-mind.firebaseapp.com',
-    messagingSenderId: '653443395788',
+    apiKey: 'AIzaSyC440kr62NRaGA4IVuJMonFcOri6lH4I-4',
+    authDomain: 'akkaraju-satvik.firebaseapp.com',
+    messagingSenderId: '17993737222',
   },
   production: true
 };
